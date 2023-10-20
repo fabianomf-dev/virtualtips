09b7d54bc258608e0f061c256cf087f7a0fed62e
fabianomf-dev <fabiano.silva.dev@gmail.com>
2023-10-20 03:49:20 -0300

    ok

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -6,8 +6,7 @@ import type { NextRequest,NextResponse } from 'next/server'
 import requestIp from 'request-ip';
 export const runtime = 'edge'
 
-export async function GET(request: NextRequest,response:NextResponse) {
-  console.log(requestIp.getClientIp(response))
+export async function GET(request: NextRequest) {
   const userIP = requestIp.getClientIp(request);
-  return new Response(JSON.stringify({ name: 'John Doe', userIP }));
+  return new Response(JSON.stringify({ userIP }));
 }
