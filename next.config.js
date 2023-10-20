5d08eeba5d19d36140aa46cef5d2fc19aa77a88d
fabianomf-dev <fabiano.silva.dev@gmail.com>
2023-10-20 15:07:31 -0300

    new image

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // Desabilitar ESLint no ambiente de produção
-        ignoreDuringBuilds: process.env.NODE_ENV === 'production',
-      }
+  eslint: {
+    // Desabilitar ESLint no ambiente de produção
+    ignoreDuringBuilds: process.env.NODE_ENV === 'production',
+  },
+  images: {
+    disableStaticImages: true,
+  },
 }
 
 module.exports = nextConfig
