ac438aed17acba9c7101a3290dc26484baab53b3
fabianomf-dev <fabiano.silva.dev@gmail.com>
2023-10-20 03:50:38 -0300

    ok

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -1,12 +1,10 @@
 
 // Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
 
-import type { NextRequest,NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-import requestIp from 'request-ip';
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  const userIP = requestIp.getClientIp(request);
-  return new Response(JSON.stringify({ userIP }));
+  return new Response(JSON.stringify({ name: 'John Doe' }))
 }
